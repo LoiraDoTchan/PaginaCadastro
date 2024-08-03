5889519b499d68609466053205bb578392957d2e
Mel Raposeiras <134176134+fraise-renard@users.noreply.github.com>
2024-08-03 12:25:05 -0300

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,12 @@ $email = NULL;
 $senha = NULL;
 if(isset ($_POST['email']) and isset ($_POST['senha'])){
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $senha = $_POST['senha'];
+
+    //connection
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
     $conn = new mysqli($servername, $username, $password);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
